afdb256e9c09020703100d4526d8107ab63edd85
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-05 21:50:55 +0000

    redui le un tout petit, encore (_for element <Logo>_)

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative h-24 w-24 flex items-center justify-center bg-black/80 rounded-full", className)}>
-      <div className="relative h-20 w-20">
+    <div className={cn("relative h-20 w-20 flex items-center justify-center bg-black/80 rounded-full", className)}>
+      <div className="relative h-16 w-16">
         <Image 
           src="/logo.png" 
           alt="Danny Store Logo"
