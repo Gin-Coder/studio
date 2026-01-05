28bf578298442ae3f613a7b6b60c521f3b9aba19
Your Name <ggfdaniel179@gmail.com>
2026-01-05 22:17:10 +0000

    ne laisse pas ce cercle depasser le navbar (_for element img_)

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative h-20 w-20 flex items-center justify-center bg-black/80 rounded-full", className)}>
-      <div className="relative h-[72px] w-[72px]">
+    <div className={cn("relative h-12 w-12 flex items-center justify-center bg-black/80 rounded-full", className)}>
+      <div className="relative h-10 w-10">
         <Image 
           src="/logo.png" 
           alt="Danny Store Logo"
