06ea855305f5a31a2d90f122f0075ec21e4a169d
Your Name <ggfdaniel179@gmail.com>
2026-01-05 22:24:30 +0000

    le logo n'est pas assez grand dans le cercle dans la section hero

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative h-12 w-12 flex items-center justify-center bg-black/80 rounded-full", className)}>
-      <div className={cn("relative h-11 w-11", className?.includes('!h-24') && '!h-22 !w-22')}>
+      <div className={cn("relative h-11 w-11", className?.includes('!h-24') && '!h-[5.5rem] !w-[5.5rem]')}>
         <Image 
           src="/logo.png" 
           alt="Danny Store Logo"
