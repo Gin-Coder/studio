dda2a6726535a7df7882ace0ac76e171b9bdb577
Your Name <ggfdaniel179@gmail.com>
2026-01-05 22:18:16 +0000

    mais agrandit le logo a l'interieur du cercle

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative h-12 w-12 flex items-center justify-center bg-black/80 rounded-full", className)}>
-      <div className="relative h-10 w-10">
+      <div className="relative h-11 w-11">
         <Image 
           src="/logo.png" 
           alt="Danny Store Logo"
