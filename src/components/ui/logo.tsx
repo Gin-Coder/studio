5bc6514a24960dac2bd08c200fb820d087481feb
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-05 04:05:04 +0000

    J'ai pris soins d'ajouter mon logo dans le dossier public

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -4,26 +4,13 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative h-12 w-28", className)}>
-      {/* Light mode logo */}
-      <div className="block dark:hidden">
-        <Image 
-          src="https://storage.googleapis.com/studioprompt-helper-images/danny-store-logo-for-light-bg.png" 
-          alt="Danny Store Logo"
-          fill
-          style={{ objectFit: 'contain' }}
-          unoptimized
-        />
-      </div>
-      {/* Dark mode logo */}
-      <div className="hidden dark:block">
-        <Image 
-          src="https://storage.googleapis.com/studioprompt-helper-images/danny-store-logo-for-dark-bg.png" 
-          alt="Danny Store Logo"
-          fill
-          style={{ objectFit: 'contain' }}
-          unoptimized
-        />
-      </div>
+      <Image 
+        src="/logo.png" 
+        alt="Danny Store Logo"
+        fill
+        style={{ objectFit: 'contain' }}
+        unoptimized
+      />
     </div>
   );
 }
