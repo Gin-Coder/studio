deebdba782ea4bf5a8dbbfd958465c515b0031fc
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-06 04:23:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7666210400-60635",
+  "appId": "1:1037145277138:web:dc0e27e976d65519b1a30b",
+  "apiKey": "AIzaSyADY0n9ezmjhodHR5-zmDnRm8qz3Zt-mvg",
+  "authDomain": "studio-7666210400-60635.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1037145277138"
+};
