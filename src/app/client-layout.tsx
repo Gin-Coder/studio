f3cf330ba3ba739c3f5ca72b32fe0f3350e7ea2a
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-06 04:27:23 +0000

    .

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, type ReactNode } from 'react';
 import { useLanguage, LanguageProvider } from '@/hooks/use-language';
 import { ThemeProvider } from 'next-themes';
 import { CartProvider } from '@/hooks/use-cart';
@@ -8,7 +8,7 @@ import { WishlistProvider } from '@/hooks/use-wishlist';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 import { Toaster } from '@/components/ui/toaster';
-
+import { FirebaseClientProvider } from '@/firebase';
 
 function LanguageAttributeUpdater() {
   const { language } = useLanguage();
@@ -21,23 +21,25 @@ function LanguageAttributeUpdater() {
 }
 
 // This component runs only on the client and provides all the context.
-export function ClientLayout({ children }: { children: React.ReactNode }) {
+export function ClientLayout({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <LanguageProvider>
-        {/* The component below will update the lang attribute, and it needs to be a child of LanguageProvider */}
-        <LanguageAttributeUpdater />
-        <CartProvider>
-          <WishlistProvider>
-            <div className="flex min-h-screen flex-col">
-              <Header />
-              <main className="flex-grow">{children}</main>
-              <Footer />
-            </div>
-            <Toaster />
-          </WishlistProvider>
-        </CartProvider>
-      </LanguageProvider>
-    </ThemeProvider>
+    <FirebaseClientProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <LanguageProvider>
+          {/* The component below will update the lang attribute, and it needs to be a child of LanguageProvider */}
+          <LanguageAttributeUpdater />
+          <CartProvider>
+            <WishlistProvider>
+              <div className="flex min-h-screen flex-col">
+                <Header />
+                <main className="flex-grow">{children}</main>
+                <Footer />
+              </div>
+              <Toaster />
+            </WishlistProvider>
+          </CartProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </FirebaseClientProvider>
   );
 }
