b138a51a65f7cfc389785d5c13f47774f96989ea
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-18 05:40:03 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -39,6 +39,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
