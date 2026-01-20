250d25cd36613fcae22eb41c8ae18e6e849f5f87
Gin-Fao Daniel Georges <ggfdaniel179@gmail.com>
2026-01-20 15:51:42 +0000

    Set up a Firebase project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-7666210400-60635",
-  "appId": "1:1037145277138:web:dc0e27e976d65519b1a30b",
-  "apiKey": "AIzaSyADY0n9ezmjhodHR5-zmDnRm8qz3Zt-mvg",
-  "authDomain": "studio-7666210400-60635.firebaseapp.com",
-  "storageBucket": "studio-7666210400-60635.appspot.com",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   "measurementId": "",
-  "messagingSenderId": "1037145277138"
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
